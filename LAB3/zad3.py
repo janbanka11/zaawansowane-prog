ae88e5be4320e5a21d7ec12849ff5cfd85168011
Jan Banka <janbnk90@gmail.com>
2025-01-11 11:15:57 +0100

    zad3 partial

---
+++ LAB3/zad3.py
@@ -0,0 +1,29 @@
+from doctest import master
+
+
+class Property:
+    def __init__(self, area, rooms:int, price, address):
+        self.area = area
+        self.rooms = rooms
+        self.price = price
+        self.address = address
+    def __str__(self):
+        return f"Property area:{self.area}, rooms:{self.rooms}, price:{self.price}, address:{self.address}\n"
+
+class House(Property):
+    def __init__(self, plot):
+        self.plot = plot
+        Property.__init__(self, self.area, self.rooms, self.price, self.address)
+    def __str__(self):
+        return f"House plot:{self.plot}"
+
+class Flat(Property):
+    def __init__(self, floor):
+        self.floor = floor
+    def __str__(self):
+        return f"Flat floor:{self.floor}"
+
+house = House(1000)
+flat = Flat(3)
+print(house)
+print(flat)

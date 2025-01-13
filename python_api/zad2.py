482a60f38db71b173d6787920a138339bf8d31fe
Jan Banka <janbnk90@gmail.com>
2025-01-13 19:43:53 +0100

    pep8

--- python_api/zad2.py
+++ python_api/zad2.py
@@ -2,21 +2,24 @@ from flask import Flask
 from flask_restful import Resource, Api
 import csv
 
+
 class Movie:
     def __init__(self, id, title, genres):
         self.id = id
         self.title = title
         self.genres = genres
+
+
 class Movies:
     def __init__(self):
-        with open('movies.csv', newline='') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        with open("movies.csv", newline="") as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=" ", quotechar="|")
             i = 0
             for row in spamreader:
                 i = i + 1
                 print(i)
-                print(row   )
-        #self.movies = movies
+                print(row)
+        # self.movies = movies
 
 
 Movies = Movies()
